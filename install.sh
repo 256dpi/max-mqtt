2edc93907aa8e21c6c4360e07fd8752c9af7329b
Joël Gähwiler <joel.gaehwiler@gmail.com>
2015-06-26 11:10:53 +0200

    copy help too

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 
 cp out/production/max-mqtt/mqtt.class /Applications/Max.app/Contents/Resources/C74/java-classes/classes
 cp lib/org.eclipse.paho.client.mqttv3-1.0.2.jar /Applications/Max.app/Contents/Resources/C74/java-classes/lib
+cp help/mqtt.maxhelp /Applications/Max.app/Contents/Resources/C74/java-classes/help
