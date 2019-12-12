ba2f0f2f1ba19588a4bc3933d6abf476526c45d4
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-12-12 17:57:55 +0100

    use java 8

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-javac -d ./out -cp ".:./lib/mqtt.jar:/Applications/Max.app/Contents/Resources/C74/packages/max-mxj/java-classes/lib/max.jar" src/mqtt.java
+/Library/Java/JavaVirtualMachines/jdk1.8.0_191.jdk/Contents/Home/bin/javac -d ./out -cp ".:./lib/mqtt.jar:/Applications/Max.app/Contents/Resources/C74/packages/max-mxj/java-classes/lib/max.jar" src/mqtt.java
